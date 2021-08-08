639f63a10e47c96ef4afbd74ccae4e644d12d6ca
Hiba-Almade <hibaalmade998@gmail.com>
2021-08-08 18:41:58 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import City from './component/City';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      lat:"0.0",
+      lon:"0.0",
+      cityName:"",
+      img: "" ,
+      handleRender: false,
+      errorMsg:false
+    }
+  }
+
+  getUserInputHandler=(e)=>{
+    this.setState({
+      cityName:e.target.value
+    })
+    console.log(this.state.cityName);
+  }
+
+  submitHandler=(e)=>{
+    e.preventDefault();
+    let url=`https://eu1.locationiq.com/v1/search.php?key=pk.3bdf6b99a25ebd79aa8617d4d81d3da8&q=${this.state.cityName}&format=json`
+   try {axios.get(url).then(res=>{
+      let data= res.data[0]
+
+      this.setState({
+        cityName:data.display_name,
+        lat:data.lat,
+        lon:data.lon,
+        img : `https://maps.locationiq.com/v3/staticmap?key=pk.3bdf6b99a25ebd79aa8617d4d81d3da8&center=${data.lat},${data.lon}`,
+        handleRender: true
+      })
+    
+    });
+  }catch{
+    this.setState({
+      errorMsg:true
+    })
+  }
+  }
+  render() {
+    return (
+      <div>
+        <h1 class="text-center text-primary">LocationIQ API</h1>
+
+        
+        <form onSubmit={(e)=>this.submitHandler(e)}>
+          <input type="text" onChange={(e)=>{this.getUserInputHandler(e)}} placeholder="Search by City name" class="form-control col-6"/>
+          <input type="submit" value="Explore!" class="btn btn-primary btn-lg active mx-auto"/>
+        </form>
+        <City cityName={this.state.cityName} lat={this.state.lat} lon={this.state.lon} img = {this.state.img} handleRender = {this.state.handleRender} errorMsg ={this.state.errorMsg}/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
+
---
+++ src/component/City.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export class City extends Component {
+    render() {
+        return (
+            <div>
+               
+             {   this.props.handleRender&&
+                <div class="card border-primary mx-auto" style={{ width: '60rem' }}>
+                <div class="card-body">
+                <h4 class="card-title text-center">{this.props.cityName}</h4>
+                <p class="card-text text-center">{this.props.lat}</p>
+                <p class="card-text text-center">{this.props.lon}</p>
+                </div>
+                <img src ={this.props.img} alt = 'map' class="card-img rounded mx-auto d-block" width = "400px" height = "500px"/>
+                </div>
+                }
+
+                {this.props.errorMsg && 
+                <h3 class="border border-danger text-danger text-center">Something Rong</h3>
+                }
+            </div>
+        )
+    }
+}
+
+export default City
